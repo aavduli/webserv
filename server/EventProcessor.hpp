445da220453b5619c4e79ccbd8680289059f382c
aavduli <aavduli@42lausanne.ch>
2025-10-01 16:50:23 +0200

    Event processor

--- server/EventProcessor.hpp
+++ server/EventProcessor.hpp
@@ -4,10 +4,34 @@
 
 #include "../console/console.hpp"
 #include <iostream>
-#include "event/eventManager.hpp"
+#include "eventManager.hpp"
+#include "ConnectionManager.hpp"
+#include "server.hpp"
 
-class EventProcessor {
+class eventProcessor {
 	private:
+		eventManager& _eventManager;
+		connectionManager& _connectionManager;
+		RequestProcessor& _requestProcessor; //todo
+		int _serverFd;
+
+	private:
+		void acceptNewConnections();
+		void processIncomingData(int clientFd, const WebservConfig& config);
+		void handleReceiveError(int clientFd, ssize_t recvResult);
+		void sendResponse(int clientFd, const std::string response);
 
 	public:
+	eventProcessor(eventManager& em, connectionManager& cm, int serverFd);
+	~eventProcessor();
+
+	void runEventLoop(const Webservconfig& config);
+	void stopEventLoop();
+
+	void handleServerEvents(int serverFd);
+	void handleClientDisconnection(int clientFd); //EPOLLUP | EPOLLERR | EPOLLRDUP
+	void handleServerDisconnection(int serverFd);
+
+	
+
 };
