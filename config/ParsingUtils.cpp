a2d87ad172dd97a967397f42619bce6a04927e5e
aavduli <aavduli@42lausanne.ch>
2025-10-13 10:01:13 +0200

    cleanning + parsingutils ameliored

--- config/ParsingUtils.cpp
+++ config/ParsingUtils.cpp
@@ -6,7 +6,7 @@
 /*   By: jim <jim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 15:29:22 by jim               #+#    #+#             */
-/*   Updated: 2025/09/22 15:53:54 by jim              ###   ########.fr       */
+/*   Updated: 2025/09/22 16:58:44 by jim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <cctype>
 #include <cstdlib>
 
-static std::string trim(const std::string& str){
+std::string ParsingUtils::trim(const std::string& str) const {
 	size_t start = str.find_first_not_of("\t\n\r");
 	if (start == std::string::npos) return "";
 
@@ -24,7 +24,7 @@ static std::string trim(const std::string& str){
 	return str.substr(start, end - start + 1);
 }
 
-static std::vector<std::string> split(const std::string& str, char delimiter){
+std::vector<std::string> ParsingUtils::split(const std::string& str, char delimiter) const {
 	std::vector<std::string> tokens;
 	std::stringstream ss(str);
 	std::string token;
@@ -36,7 +36,7 @@ static std::vector<std::string> split(const std::string& str, char delimiter){
 	return tokens;
 }
 
-static std::vector<std::string> split(const std::string& str, const std::string& delimiter){
+std::vector<std::string> ParsingUtils::split(const std::string& str, const std::string& delimiter) const{
 	std::vector<std::string> tokens;
 	size_t start = 0;
 	size_t found = 0;
@@ -50,11 +50,15 @@ static std::vector<std::string> split(const std::string& str, const std::string&
 	return tokens;
 }
 
-static bool isValidIP(const std::string& ip){
+bool ParsingUtils::isValidIP(const std::string& ip)const{
 	std::vector<std::string> parts = split(ip, '.');
 	if (parts.size() != 4) return false;
 
 	for (size_t i = 0; i < parts.size(); i++){
+		for (size_t j = 0; j < parts[i].length(); j++){
+			if (!std::isdigit(parts[i][j])) return false;
+		}
+
 		int num = std::atoi(parts[i].c_str());
 		if (num < 0 || num > 255) return false;
 
@@ -63,22 +67,46 @@ static bool isValidIP(const std::string& ip){
 	return true;
 }
 
-static bool isValidPort(const std::string& port){
+bool ParsingUtils::isValidPort(const std::string& port)const{
 	if (port.empty()) return false;
 
+	for (size_t i = 0; i < port.length(); i++){
+		if (!std::isdigit(port[i])) return false;
+	}
 	int num = std::atoi(port.c_str());
 	return (num >= 1 && num <= 65535);
 }
 
-static std::string toLowerCase(const std::string& str){
+std::string ParsingUtils::toLowerCase(const std::string& str)const{
 	std::string res = str;
 	std::transform(res.begin(), res.end(), res.begin(), :: tolower);
 	return res;
 }
 
-static bool isValidMethod(const std::string method){
+bool ParsingUtils::isValidMethod(const std::string method)const{
 	std::string meth = method;
 	std::transform(meth.begin(), meth.end(), meth.begin(), ::toupper);
 
 	return(meth == "GET" || meth == "POST" ||meth == "DELETE");
+}
+
+size_t ParsingUtils::parseSize(const std::string& sizeStr) const{
+	if (sizeStr.empty()) return 0;
+
+	std::string numPart = sizeStr;
+	size_t multiplier = 1;
+
+	char lastChar = sizeStr[sizeStr.length() - 1];
+	if (!std::isdigit(lastChar)) {
+		numPart = sizeStr.substr(0, sizeStr.length() - 1);
+		switch (lastChar) {
+			case 'K': case 'k': multiplier = 1024; break;
+			case 'M': case 'm': multiplier = 1024 * 1024; break;
+			case 'G': case 'g': multiplier = 1024 * 1024 * 1024; break;
+			default: return 0;
+		}
+	}
+
+	size_t value = std::atoi(numPart.c_str());
+	return value * multiplier;
 }
