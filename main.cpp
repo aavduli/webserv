4cf2778254e50814988610b3a4e412999334ab05
jim <jimmy.roulet@gmail.com>
2025-10-20 16:54:36 +0200

    refactor bc of configfiles changement de dossier (vous avec compris quoi)

--- main.cpp
+++ main.cpp
@@ -5,38 +5,14 @@
 
 const bool PRINTCONFIG = false;
 
-int testCGI(){
-	HttpRequest request;
-	request.setMethod("GET");
-
-	RequestUri uri("/cgi_bin/hello.py?name=jim&age=42");
-	uri.parse();
-	request.setUri(uri);
-
-	WebservConfig config;
-	config.loadConfig("./config/test.conf");
-
-	CgiExec executor("./www/cgi/script.py", "/usr/bin/python3", &config);
-	std::string output = executor.execute(&request);
-
-	std::cout << "=== CGI OUTPUT ===" << std::endl;
-	std::cout << output << std::endl;
-	std::cout << "================" << std::endl;
-
-	return 0;
-
-}
-
 int main(int ac, char **av) {
-
-	//testCGI();
 	console::openFile();
 	if (ac != 2){
 		console::log("Config file error ", ERROR);
 		return 1;
 	}
 	std::string filename = av[1];
-	std::string fn = "./config/" + filename;
+	std::string fn = "./configfiles/" + filename;
 
 	if (fn.find(".conf") == std::string::npos){
 		fn += ".conf";
