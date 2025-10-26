442b502960a541bbc072c4c26204a52b0522ab9e
jim <jimmy.roulet@gmail.com>
2025-10-26 17:12:44 +0100

    fix security in config and cgi

--- cgi/CgiExec.hpp
+++ cgi/CgiExec.hpp
@@ -6,7 +6,7 @@
 /*   By: jim <jim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 15:20:25 by jim               #+#    #+#             */
-/*   Updated: 2025/10/13 15:01:01 by jim              ###   ########.fr       */
+/*   Updated: 2025/10/26 15:55:08 by jim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ class CgiExec
 		std::string _script_path;
 		std::string _python_path;
 		const WebservConfig* _config;
+
+		bool isValidScriptPath(const std::string& script_path, const std::string& doc_root);
 
 
 	public:
