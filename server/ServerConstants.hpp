cb30384d63b28b43c93912fc84f04f1f69063570
aavduli <aavduli@42lausanne.ch>
2025-11-03 15:31:56 +0100

    server is ready to pushed.

--- server/ServerConstants.hpp
+++ server/ServerConstants.hpp
@@ -7,7 +7,6 @@ namespace ServerConstants {
 
 	// Buffer sizes
 	static const size_t BUFFER_SIZE = 8192;
-	static const size_t MAX_REQUEST_SIZE = 1048576;  // 1MB
 	static const size_t MAX_HEADER_SIZE = 1048576;
 	static const int MAX_FILENAME_COUNTER = 10000;
 	
